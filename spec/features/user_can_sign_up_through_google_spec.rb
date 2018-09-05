b85fcd5b11a08f698cd2fdbac67a7a87603a6aef
ClaireBvs <clairembeauvais@gmail.com>
2018-09-05 17:52:09 -0600

    add feature test sign up with google

---
+++ spec/features/user_can_sign_up_through_google_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'a visitor' do
+  context 'visit the root path' do
+    it 'can click on a Sign up button to go to dashboard page' do
+      user_name = "Henry"
+
+      visit root_path
+
+      click_link 'Sign Up with Google'
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("Welcome, #{user_name}")
+      expect(page).to_not have_link('Log In')
+      expect(page).to have_link('Log Out')
+    end
+  end
+end 
