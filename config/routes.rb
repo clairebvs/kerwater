ce8e4bd4b04aea1b739c0e689ede357f61a75cc5
ClaireBvs <clairembeauvais@gmail.com>
2018-09-06 17:01:36 -0600

    add User table to database

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
 
+  get '/auth/google_oauth2/callback', to: 'sessions#create'
+
   get 'dashboard', to: 'dashboard#show'
 
 end
