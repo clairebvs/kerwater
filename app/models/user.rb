7d5fb1725be68ec353a1a316d3435a182be71956
ClaireBvs <clairembeauvais@gmail.com>
2018-09-06 17:22:34 -0600

    add method current_user, from_omniauth and action create and destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.username = auth.info.username
+      user.oauth_token = auth.credentials.token
+      user.save!
+    end
+  end
 end
