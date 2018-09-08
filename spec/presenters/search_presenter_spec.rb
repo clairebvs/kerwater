a51a857679c101a33fdfd2f36a13781aef5b1535
ClaireBvs <clairembeauvais@gmail.com>
2018-09-07 18:56:49 -0600

    add test for instance method projects

--- spec/presenters/search_presenter_spec.rb
+++ spec/presenters/search_presenter_spec.rb
@@ -5,4 +5,15 @@ describe SearchPresenter do
     search_presenter = SearchPresenter.new
     expect(search_presenter).to be_a(SearchPresenter)
   end
+
+  context 'instance methods' do
+    context '#projects' do
+      it 'returns all projects with a link to water' do
+        search_presenter = SearchPresenter.new
+
+        expect(search_presenter.projects.count).to eq(10)
+        expect(search_presenter.projects.first).to be_a(Project)
+      end
+    end
+  end
 end
