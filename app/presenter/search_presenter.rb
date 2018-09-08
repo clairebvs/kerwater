dc376419e3b017a84604b7efc95c568730e43084
ClaireBvs <clairembeauvais@gmail.com>
2018-09-07 19:01:29 -0600

    add api call to projects method

--- app/presenter/search_presenter.rb
+++ app/presenter/search_presenter.rb
@@ -1,2 +1,14 @@
 class SearchPresenter
+  def projects
+    conn = Faraday.new(url: "https://search.worldbank.org") do |faraday|
+      faraday.headers["X-api-Key"] = ENV['DEV-KEY']
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = conn.get("/api/v2/projects?format=json&theme_exact=Water+resource+management&source=IBRD&kw=N")
+
+    JSON.parse(response.body, symbolize_names: true)[:projects].map do |project_data|
+      Project.new(project_data)
+    end
+  end
 end
