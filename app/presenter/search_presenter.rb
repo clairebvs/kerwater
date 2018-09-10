b1b621e99da3b81a8c9f9baff6095f834c810f99
ClaireBvs <clairembeauvais@gmail.com>
2018-09-09 18:48:33 -0600

    add conn api to world bank service

--- app/presenter/search_presenter.rb
+++ app/presenter/search_presenter.rb
@@ -4,13 +4,13 @@ class SearchPresenter
     service.water_projects.map do |water_project|
       Project.new(water_project)
     end
-    
-    conn = Faraday.new(url: "https://search.worldbank.org") do |faraday|
-      faraday.headers["X-api-Key"] = ENV['DEV-KEY']
-      faraday.adapter Faraday.default_adapter
-    end
 
-    response = conn.get("/api/v2/projects?format=json&theme_exact=Water+resource+management&source=IBRD&kw=N")
+    # conn = Faraday.new(url: "https://search.worldbank.org") do |faraday|
+    #   faraday.headers["X-api-Key"] = ENV['DEV-KEY']
+    #   faraday.adapter Faraday.default_adapter
+    # end
+
+    # response = conn.get("/api/v2/projects?format=json&theme_exact=Water+resource+management&source=IBRD&kw=N")
 
     JSON.parse(response.body, symbolize_names: true)[:projects].map do |id, project_data|
        Project.new(project_data)
