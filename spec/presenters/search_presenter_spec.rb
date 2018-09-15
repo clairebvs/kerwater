12ca0f829060890973d01440b33ab84357ee476b
ClaireBvs <clairembeauvais@gmail.com>
2018-09-15 12:06:26 -0600

    add presenter test for #locations of SearchPresenter

--- spec/presenters/search_presenter_spec.rb
+++ spec/presenters/search_presenter_spec.rb
@@ -1,18 +1,25 @@
 require 'rails_helper'
 
 describe SearchPresenter do
+  subject { SearchPresenter.new }
+
   it 'exists' do
-    search_presenter = SearchPresenter.new
-    expect(search_presenter).to be_a(SearchPresenter)
+    expect(subject).to be_a(SearchPresenter)
   end
 
   context 'instance methods' do
     context '#projects' do
       it 'returns all projects with a link to water' do
-        search_presenter = SearchPresenter.new
 
-        expect(search_presenter.projects.count).to eq(10)
-        expect(search_presenter.projects.first).to be_a(Project)
+        expect(subject.projects.count).to eq(10)
+        expect(subject.projects.first).to be_a(Project)
+      end
+    end
+    context '#locations' do
+      it 'returns the location for all projects' do
+
+        expect(subject.locations.count).to eq 8
+        expect(subject.locations.first).to be_a(Location)
       end
     end
   end
