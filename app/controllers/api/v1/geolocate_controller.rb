fa4a0f28e0690378becb14630bc8b7fa1240fa63
ClaireBvs <clairembeauvais@gmail.com>
2018-09-15 18:03:50 -0600

    wip try render geolocation and projects information

--- app/controllers/api/v1/geolocate_controller.rb
+++ app/controllers/api/v1/geolocate_controller.rb
@@ -3,8 +3,18 @@ class Api::V1::GeolocateController < ApplicationController
   def index
     # google_service = GoogleGeocodeService.new
     geo = SearchPresenter.new
+    @geolocation = geo.locations
+    @projects = geo.projects
+  
+    respond_to do |format|
+      format.json { render :json =>
+        { :geolocation => @geolocation,
+          :projects => @projects
+        }
+      }
+    end
     # render json: google_service.coordinates
-    render json: geo.locations
+    # render json: geo.locations
   end
 
 end
