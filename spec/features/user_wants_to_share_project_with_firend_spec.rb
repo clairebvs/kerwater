e5828a88b956fba080908a5fdd70ed47f5eceac8
ClaireBvs <clairembeauvais@gmail.com>
2018-09-19 08:28:30 -0600

    add feature test to send sms to friend

---
+++ spec/features/user_wants_to_share_project_with_firend_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+
+feature 'a logged in user' do
+  scenario 'wants to share a project with a friend by filling a form to add their phone number' do
+    project_1 = create(:project)
+
+    visit project_path(project_1)
+
+    expect(current_path).to eq(project_path(project_1))
+
+    fill_in :phone_number, with: '970-231-1234'
+
+    click_on 'Send Text'
+    # And click a “Send Text” Button
+    # The phone number will be texted the project name and a link to that projects show page.
+  end
+end
