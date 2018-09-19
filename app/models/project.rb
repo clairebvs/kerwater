1919bebaf0603a33a505439b284df9880e701ef1
ClaireBvs <clairembeauvais@gmail.com>
2018-09-18 19:38:44 -0600

    add method to check region name

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,22 +7,33 @@ class Project < ApplicationRecord
     database_project_objects = Project.all
     Project.check_project_name(api_data_objects, database_project_objects)
     Project.check_country_name(api_data_objects, database_project_objects)
+    Project.check_region(api_data_objects, database_project_objects)
   end
 
-  def self.check_project_name(api_data_objects, database_project_objects)
-    api_data_objects.map do |key, value|
-      if value[:project_name] != database_project_objects
+  def self.check_project_name(api_data, database_data)
+    api_data.map do |key, value|
+      if value[:project_name] != database_data
         Project.update(name: value[:project_name])
       end
     end
   end
 
-  def self.check_country_name(api_data_objects, database_project_objects)
-    api_data_objects.map do |key, value|
-      if value[:countryshortname] != database_project_objects
+  def self.check_country_name(api_data, database_data)
+    api_data.map do |key, value|
+      if value[:countryshortname] != database_data
         Project.update(country: value[:countryshortname])
       end
     end
   end
+
+  def self.check_region(api_data, database_data)
+    api_data.map do |key, value|
+      if value[:regionname] != database_data
+        Project.update(region: value[:regionname])
+      end
+    end
+  end
+
+  
 
 end
